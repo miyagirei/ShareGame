8126eb633bbcee37b65d862af949c65d5f5bcd00
hirarium <h1r415h1n.mcplr1029@gmail.com>
2025-09-12 00:09:55 +0900

    シーン遷移処理の基盤

--- ShareGame/MapCreate.cpp
+++ ShareGame/MapCreate.cpp
@@ -1,18 +1,59 @@
 #include "Dxlib.h"
 #include "MapCreate.h"
 
-mapNum map = map1;
+mapNum mapState = map1;
+
+bool isClicked() {
+	static int prevMouse = 0;
+	int mouseInput = GetMouseInput();
+
+	bool clicked = ((mouseInput & MOUSE_INPUT_LEFT) && !(prevMouse & MOUSE_INPUT_LEFT));
+	if(clicked) DrawString(100, 100, "clicked", GetColor(255, 255, 255));
+
+	prevMouse = mouseInput;
+	return clicked;
+}
 
 void SceneChange() {
-	if (map == map1) {
-		DrawFormatString(0, 0, GetColor(255, 255, 255), "aaa");
+
+	if (mapState == map1) {
+		DrawFormatString(50, 50, GetColor(255, 255, 255), "aaa");
+		DrawString(200, 200, "map1", GetColor(255, 255, 255));
+		if (isClicked()) {
+			mapState = map2;
+		}
 	}
 
-	else if (map == map2) {
-		DrawFormatString(0, 0, GetColor(255, 255, 255), "bbb");
+	else if (mapState == map2) {
+		DrawFormatString(50, 50, GetColor(255, 255, 255), "bbb");
+		DrawString(200, 200, "map2", GetColor(255, 255, 255));
+		if (isClicked()) {
+			mapState = map3;
+		}
 	}
 
-	else if (map == map3) {
-		DrawFormatString(0, 0, GetColor(255, 255, 255), "ccc");
+	else if (mapState == map3) {
+		DrawFormatString(50, 50, GetColor(255, 255, 255), "ccc");
+		DrawString(200, 200, "map3", GetColor(255, 255, 255));
+		if (isClicked()) {
+			mapState = map1;
+		}
 	}
 }
+
+//atodekesite
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	if (DxLib_Init() == -1) return -1;
+	SetDrawScreen(DX_SCREEN_BACK);
+	while (ProcessMessage() == 0)
+	{
+		ClearDrawScreen();
+		SceneChange();
+		ScreenFlip();
+	}
+	
+	DxLib_End();
+	return 0;
+}
+
