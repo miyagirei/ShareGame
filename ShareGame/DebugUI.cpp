17d146a55f9273c40e72f92a9cf03075d1c9ee6e
hirarium <h1r415h1n.mcplr1029@gmail.com>
2025-09-12 23:48:22 +0900

    デバッグ用の関数定義のみ

---
+++ ShareGame/DebugUI.cpp
@@ -0,0 +1,22 @@
+#include "DebugUI.h"
+#include "DxLib.h"
+
+bool DebugUI::isDebugKeyPressed() {
+
+}
+
+bool DebugUI::isClicked() {
+
+}
+
+void DebugUI::SummonDebug() {
+
+}
+
+void DebugUI::SceneTranceButton() {
+
+}
+
+void DebugUI::EnableGrid() {
+
+}
---
+++ ShareGame/DebugUI.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class DebugUI {
+public:
+	void SummonDebug();
+	void SceneTranceButton();
+	void EnableGrid();
+
+private:
+	bool isClicked();
+	bool isDebugKeyPressed();
+};
